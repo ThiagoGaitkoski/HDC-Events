428534375fa6278432e42170d9bf7df2ef287928
Thiago Gaitkoski <68345301+ThiagoGaitkoski@users.noreply.github.com>
2021-03-17 11:40:24 -0300

    Criação de model e chamada de evento pro index

--- HDC-Events/app/Http/Controllers/EventController.php
+++ HDC-Events/app/Http/Controllers/EventController.php
@@ -4,10 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Event; //Chamada dos eventos
+
 class EventController extends Controller
 {
     public function index(){
-        return view('welcome');
+
+        $events = Event::all();
+
+        return view('welcome',
+        [
+            'events' => $events //Retorno dos eventos no index
+        ]);
     }
 
     public function create(){
