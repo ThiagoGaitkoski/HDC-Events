cbd251a54f6386e4338b62eef38d4fdf29c23692
Thiago Gaitkoski <68345301+ThiagoGaitkoski@users.noreply.github.com>
2021-03-17 17:31:29 -0300

    Upload de foto no formulario para os eventos

--- HDC-Events/resources/views/events/create.blade.php
+++ HDC-Events/resources/views/events/create.blade.php
@@ -6,8 +6,13 @@
 
     <div id="event-create-container" class="col-md-6 offset-md-3">
         <h1>Crie seu evento</h1>
-        <form action="/events" method="POST">
+        <form action="/events" method="POST" enctype="multipart/form-data">
         @csrf<!--SÓ ENVIA O FORM COM ESSA DIRETIVA / PREVENÇÃO DE ATAQUES A FORMULÁRIOS-->
+            <div class="form-group">
+                <label for="image">Imagem do Envento:</label>
+                <input type="file" id="image" name="image" class="form-control-file">
+            </div><!--form-group-->
+
             <div class="form-group">
                 <label for="title">Evento:</label>
                 <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
